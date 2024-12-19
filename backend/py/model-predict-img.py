f0af5a71e96a26e52936dc5e23a732c243d141a5
heysonanderson <michaelzz2004@gmail.com>
2024-12-19 11:32:06 +0300

    3rd

--- backend/py/model-predict-img.py
+++ backend/py/model-predict-img.py
@@ -1,9 +1,22 @@
 from ultralytics import YOLO
 
-photo_path = ["D:/fullstack/CV/backend/predict/photo_5406897710499488781_y.jpg",
-              "D:/fullstack/CV/backend/predict/photo_5406897710499488782_y.jpg",
-              "D:/fullstack/CV/backend/predict/2024-12-17_20-59-50.png"]
-model = YOLO('runs/detect/train3/weights/last.pt')
+photo_path = ["../predict/2024-12-17_20-59-50.png",
+              "../predict/photo_5409149510313174660_y.jpg",
+              "../predict/photo_5409149510313174661_y.jpg",
+              "../predict/photo_5409149510313174663_y.jpg",
+              "../predict/photo_5409149510313174664_y.jpg",
+              "../predict/photo_5409149510313174665_y.jpg",
+              "../predict/photo_5409149510313174666_y.jpg",
+              "../predict/photo_5409149510313174667_y.jpg",
+              "../predict/photo_5409149510313174668_y.jpg",
+              "../predict/photo_5409149510313174669_y.jpg",
+              "../predict/photo_5409149510313174670_y.jpg",
+              "../predict/photo_5409149510313174671_y.jpg",
+              "../predict/photo_5409149510313174672_y.jpg",
+              "../predict/photo_5409149510313174673_y.jpg",
+              "../predict/photo_5409149510313174674_y.jpg"
+              ]
+model = YOLO('runs/detect/train/weights/best.pt')
 results = model(photo_path)  # return a list of Results objects
 
 for result in results:
--- backend/py/model-train.py
+++ backend/py/model-train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
-model = YOLO('runs/detect/train/weights/best.pt')
+model = YOLO('yolov8n.pt')
 
 ds_path = 'd:/fullstack/cv/backend/ds/Fruits&nonFruits/data.yaml'
 
-results = model.train(data = ds_path, epochs = 60, imgsz = 640)
+results = model.train(data = ds_path, epochs = 60, imgsz = 320)
